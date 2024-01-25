be99b87ff4e94d2b26d3ba80ddfa7f2b831fd590
Rostyslav <RostikL1994@gmail.com>
2024-01-25 22:20:10 +0200

    done with dropdown

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,38 @@
+// document.addEventListener('click', e => {
+// 	const isDropDownButton = e.target.matches("[data-dropdown-button]")
+
+// 	if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
+
+// 	let currentDropdown
+// 	if (isDropDownButton) {
+// 		currentDropdown = e.target.closest('[data-dropdown]')
+// 		currentDropdown.classList.toggle('dropdown__active')
+// 	}
+
+// 	document.querySelectorAll("[data-dropdown].dropdown__active").forEach(dropdown => {
+// 		if (dropdown === currentDropdown) return
+// 		dropdown.classList.remove("dropdown__active")
+// 	})
+// })
+
+
+
 document.addEventListener('click', e => {
-	const isDropDownButton = e.target.matches("[data-dropdown-button]")
+	const isDropDown = e.target.closest("[data-dropdown]");
 
-	if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
-
-	let currentDropdown
-	if (isDropDownButton) {
-		currentDropdown = e.target.closest('[data-dropdown]')
-		currentDropdown.classList.toggle('dropdown__active')
+	if (!isDropDown) {
+		// Закриття всіх випадаючих меню при кліку за межі області dropdown
+		document.querySelectorAll("[data-dropdown].dropdown__active").forEach(dropdown => {
+			dropdown.classList.remove("dropdown__active");
+		});
+		return;
 	}
 
+	const currentDropdown = isDropDown.closest('[data-dropdown]');
+	currentDropdown.classList.toggle('dropdown__active');
+
 	document.querySelectorAll("[data-dropdown].dropdown__active").forEach(dropdown => {
-		if (dropdown === currentDropdown) return
-		dropdown.classList.remove("dropdown__active")
-	})
-})
+		if (dropdown === currentDropdown) return;
+		dropdown.classList.remove("dropdown__active");
+	});
+});
